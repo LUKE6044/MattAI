43dd8b90a1c77c895634466fbeb51a99407c0bc5
LukeSecurity <143283191+LUKE6044@users.noreply.github.com>
2025-06-30 13:11:47 +0200

    Update test2.py

--- test/test2.py
+++ test/test2.py
@@ -1,6 +1,6 @@
 import torch
 
-state = torch.load("model.pth", map_location="cpu")
+state = torch.load("../model.pth", map_location="cpu")
 print("Chiavi nello state dict:")
 for k, v in state.items():
     print(f"{k}: shape {tuple(v.shape)}")
